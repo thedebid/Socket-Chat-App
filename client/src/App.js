e76887255432a3b6ffea372a9342c65eb8acff98
Debid Magar <linkwithdm@gmail.com>
2021-08-04 16:18:35 +0545

    server connection added and cors error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+let socket;
+const CONNECTION_PORT = "localhost:3002/";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [room, setRoom] = useState("");
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, []);
+
+  const connectToRoom = () => {
+    socket.emit("join_room", room);
+  };
 
   return (
     <div className="App">
       {!isLoggedIn ? (
         <div className="login">
           <div className="input">
-            <input type="text" placeholder="Name..." />
-            <input type="text" placeholder="Room..." />
+            <input
+              type="text"
+              placeholder="Name..."
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Room..."
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
           </div>
-          <button>Enter Chat</button>
+          <button onClick={connectToRoom}>Enter Chat</button>
         </div>
       ) : (
         <h1>Your are Logged In</h1>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
+const socket = require("socket.io");
 const app = express();
-const cors = require('cors');
 
-app.use(cors());
+app.use(express.json());
 
-app.listen(3002, () => {
-    console.log('Server is running on port 3002');
+const server = app.listen(3002, () => {
+  console.log("Server is running on port 3002");
+});
+
+io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("User Joined Room: " + data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
 });
